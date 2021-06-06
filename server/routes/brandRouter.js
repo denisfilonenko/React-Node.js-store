c4bb13b17e520f53923a8b2e02cd25eb95098d15
Denys Filonenko <denisfilonenko@meta.ua>
2021-06-06 20:34:21 +0300

    Added pre-version of ready server

--- server/routes/brandRouter.js
+++ server/routes/brandRouter.js
@@ -1,7 +1,6 @@
 const Router = require('express')
 const brandController = require('../controllers/brandController')
 const router = new Router()
-const BrandController = require('../controllers/brandController')
 
 router.post('/', brandController.create)
 router.get('/', brandController.getAll)
